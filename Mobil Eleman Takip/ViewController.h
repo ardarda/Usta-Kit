31033af5aea9f4bc14027bdc71b2073ab3ce0dff
Arda Cicek <ardacck@gmail.com>
2015-03-08 17:43:49 +0200

    Initial Commit

---
+++ Mobil Eleman Takip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mobil Eleman Takip
+//
+//  Created by Arda Cicek on 08/03/15.
+//  Copyright (c) 2015 Arda Cicek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mobil Eleman Takip/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mobil Eleman Takip
+//
+//  Created by Arda Cicek on 08/03/15.
+//  Copyright (c) 2015 Arda Cicek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
